02419281b08c85b36423f161e82917ed82972f30
ST10311687 <ST10311687@vcconnect.edu.za>
2024-04-14 20:38:54 +0200

    Update 1: Added basic menu for user to decide from.

--- PROG6221_POE_ST10311687/Program.cs
+++ PROG6221_POE_ST10311687/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PROG6221_POE_ST10311687.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace PROG6221_POE_ST10311687
     {
         static void Main(string[] args)
         {
+            Recipe recipe = new Recipe();
+
+            while (true)
+            {
+                Console.WriteLine("\n1. Enter Recipe Details");
+                Console.WriteLine("2. Display Recipe");
+                Console.WriteLine("3. Scale Recipe");
+                Console.WriteLine("4. Reset Quantities");
+                Console.WriteLine("5. Clear Recipe");
+                Console.WriteLine("6. Exit");
+
+                Console.Write("\nEnter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
