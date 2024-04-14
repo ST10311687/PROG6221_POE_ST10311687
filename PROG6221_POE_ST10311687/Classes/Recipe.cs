9a8ef608f0b543ed82c8af671ab3d1bf5e64dac1
ST10311687 <ST10311687@vcconnect.edu.za>
2024-04-14 20:34:29 +0200

    Add project files.

---
+++ PROG6221_POE_ST10311687/Classes/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_POE_ST10311687.Classes
+{
+    internal class Recipe
+    {
+    }
+}
