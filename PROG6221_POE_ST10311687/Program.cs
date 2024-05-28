1a4fd4e64b2e2bce73f8c0aa0b5f667c48b22af5
Lee James <ST10311687@vcconnect.edu.za>
2024-05-28 17:05:16 +0200

    Added color!

--- PROG6221_POE_ST10311687/Program.cs
+++ PROG6221_POE_ST10311687/Program.cs
@@ -16,12 +16,24 @@ namespace PROG6221_POE_ST10311687
             while (true)
             {
                 //Menu that allows user to enter a number coresponding to an action
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("\n1. Enter Recipe Details");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine("2. Display Recipe");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Console.WriteLine("3. Scale Recipe");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("4. Reset Quantities");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("5. Clear Recipe");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("6. Exit");
+                Console.ResetColor();
 
                 Console.Write("\nEnter your choice: ");
                 int choice = int.Parse(Console.ReadLine());
